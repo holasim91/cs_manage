1121637eaa3f8bc2e95aa6d6031cecc2fb15ab1d
HyunIn Sim <>
2020-03-05 16:47:23 +0900

    Add Database Connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,38 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
-import Paper from '@material-ui/core/Paper'
+import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
-import {makeStyles} from '@material-ui/core/styles'
+import { makeStyles } from "@material-ui/core/styles";
 
-
-const theme = {
-  spacing: 4,
-}
-
-
-const useStyles = makeStyles(
-  {
-  root:{
-    width:'100%',
-    overflowX: 'auto',
-    marginTop:theme.spacing
-
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    overflowX: "auto",
+    marginTop: theme.spacing(4)
   },
-  table:{
-    minWidth : 1080
+  table: {
+    minWidth: 1080
   }
-});
-
-
+}));
 
 export default function App() {
-  const classes = useStyles()
+  const classes = useStyles();
   const [customers, setCustomers] = useState([]);
-
   useEffect(() => {
     async function fetchData() {
       const res = await fetch("/api/customers");
-      res.json()
-        .then(res => setCustomers(res))
+      res.json().then(res => setCustomers(res));
     }
 
     fetchData();
-  },[]);
-  return (
+  }, []);
 
+  return (
     <Paper className={classes.root}>
       <Table className={classes.table}>
         <TableHead>
@@ -58,20 +46,21 @@ export default function App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {customers?customers.map(customer => (
-            <Customer
-              key={customer.id}
-              id={customer.id}
-              img={customer.img}
-              name={customer.name}
-              birth={customer.birth}
-              gender={customer.gender}
-              job={customer.job}
-            />
-          )):""}
+          {customers
+            ? customers.map(customer => (
+                <Customer
+                  key={customer.id}
+                  id={customer.id}
+                  img={customer.image}
+                  name={customer.name}
+                  birth={customer.birth}
+                  gender={customer.gender}
+                  job={customer.job}
+                />
+              ))
+            : "Data Fetching..."}
         </TableBody>
       </Table>
     </Paper>
   );
 }
-
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -6,33 +7,28 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json')
+const conf = JSON.parse(data)
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user : conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+})
+console.log(connection)
+connection.connect()
+
+
 app.get("/api/customers", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      img: "https://placeimg.com/64/64/1",
-      name: "Thor",
-      birth: "950413",
-      gender: "male",
-      job: "son of odin"
-    },
-    {
-      id: 2,
-      img: "https://placeimg.com/64/64/2",
-      name: "Natasha",
-      birth: "920222",
-      gender: "female",
-      job: "Black Widow"
-    },
-    {
-      id: 3,
-      img: "https://placeimg.com/64/64/3",
-      name: "Peter Quill",
-      birth: "910413",
-      gender: "male",
-      job: "Star Lord"
+  connection.query(
+    "SELECT * FROM CUSTOMER;",
+    (err,rows, fields)=>{
+      res.send(rows)
     }
-  ]);
+  )
 });
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
